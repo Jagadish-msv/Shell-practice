d9c3bf7591f7d513fbb96abff1b57fad2369da83
jagadish-msv <jagadishmadhavarapu1@gmail.com>
2026-01-29 11:25:09 +0530

    new script

---
+++ 7-specialvariables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "the current working directory $pwd "
+echo "the current home directory $HOME"
+echo " the current user is $USER"
+echo "the script PID is $$"
+echo "the backgroup process is $!"
+echo "the total number of arguments are $@"

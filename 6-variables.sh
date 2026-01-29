4bf3aaaf0b2137793c15d67b1c4223f930cea413
jagadish-msv <jagadishmadhavarapu1@gmail.com>
2026-01-29 11:11:12 +0530

    new script

--- 6-variables.sh
+++ 6-variables.sh
@@ -2,8 +2,8 @@
 
 timestamp1=$(date +%s)
 
-echo "script started at $timestamp"
+echo "script started at $timestamp1"
 sleep 10
 timestamp2=$(date +%s)
-total_time=$(($timestamp1-$timestamp2))
+total_time=$(($timestamp2-$timestamp1))
 echo "script executed in $total_time"

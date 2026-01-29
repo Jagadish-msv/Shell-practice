28b93354bffddeb9a71cec2d34c977b526b6756d
jagadish-msv <jagadishmadhavarapu1@gmail.com>
2026-01-29 10:40:59 +0530

    dynamic variables

---
+++ 4-dynamicvariables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1 ::  Hi $person2 . How are you?"
+echo "$person2 :: I am good.Thank you. How are you doing?"
+echo "$person1 :: I am the powerfull guy. SO I am good"
+echo "$person1 :: Is my help with ukraine war sufficent?"
+echo "$person2 :: you are doing more than enough. Thank you"

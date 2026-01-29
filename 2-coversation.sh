922d26821101bba0bd9290d395455de677efc0e1
jagadish-msv <jagadishmadhavarapu1@gmail.com>
2026-01-29 10:13:27 +0530

    updated script

---
+++ 2-coversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Suresh:: Hi Ramesh, How are you?"
+echo "Ramesh:: I am good. How are you doing?"
+echo "Suresh:: Iam good .Thank you."
+echo "Suresh:: What are you doing?"
+echo ""Ramesh:: I am learning shell scripting"

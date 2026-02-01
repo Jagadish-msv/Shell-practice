95b0c329397ca74cc1b0b0c042d70e3bf62e6c56
jagadish-msv <jagadishmadhavarapu1@gmail.com>
2026-02-01 16:48:29 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -5,17 +5,17 @@ AMI_ID="ami-0220d79f3f480ecf5"
 
 for instance in $@ 
 do
-    Instance_ID =$(
+    Instance_ID=$(
                     aws ec2 run-instances \
                     --image-id $AMI_ID \
                     --instance-type t3.micro \
                     --security-group-ids $SG_ID \
-                    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}] \
+                    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
                     --query 'Insatnce[0] , InstanceId' \
                     --output text 
                 )
 
-        if [$instance == "frontend"]; then
+        if [ $instance == "frontend" ]; then
             ip=$(
                     aws ec2 describe-instances \
                     --instances-ids $Instance_ID \
